e7a239c2e9cd0a11c029a929cbef6412db95c77a
ravikanur <ravikanur@gmail.com>
2022-04-22 18:23:04 +0530

    changed project structure

---
+++ src/detectron2/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from .utils.env import setup_environment
+import sys
+
+sys.path.insert(0, 'src')
+
+setup_environment()
+
+
+# This line will be programatically read/write by setup.py.
+# Leave them at the bottom of this file and don't touch them.
+__version__ = "0.6"
---
+++ src/tf2/object_detection/__init__.py
@@ -0,0 +1,3 @@
+import sys
+
+#sys.path.append('src/tf2_predictor/object_detection')
